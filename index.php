fe5c7b72dbafbb05b1488e1e4134780c82dc5669
IgorBenedito <50702677+IgorBenedito@users.noreply.github.com>
2019-05-16 09:37:25 -0300

    Add files via upload

---
+++ index.php
@@ -0,0 +1,10 @@
+<?php
+    include 'head.html';
+    include 'menu.html';
+    include 'topo.html';
+    include 'mapa.html';
+    include 'quem_somos.html';
+    include 'time.html';
+    include 'contato.html';
+    include 'rodape.html';
+?>
